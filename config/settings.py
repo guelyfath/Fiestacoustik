718b61fad6565e457d2dc88ed9d65930ca144f0a
Arthur Yohani <yohaniarthur@gmail.com>
2026-06-26 11:19:35 +0200

    Ajout whitenoise

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Charges les fichiers static en cherchant le dossier static dans tous les dossiers
 STATICFILES_DIRS = [
